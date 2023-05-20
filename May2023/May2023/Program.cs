85870a67ae6360b6106da494f271a6ed8c05695b
yvonnetce <mothercowchicken@gmail.com>
2023-05-20 16:40:31 +0800

    Cre8 Time & Material record

--- May2023/May2023/Program.cs
+++ May2023/May2023/Program.cs
@@ -12,6 +12,7 @@ public class Program
         //launch turnup portal
         driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
         driver.Manage().Window.Maximize();
+        Thread.Sleep(2000);
 
        //identify username textbox enter valid username
        IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
@@ -27,7 +28,6 @@ public class Program
             loginbutton.Click();
 
 
-
         //check if user has logged in successfully
         IWebElement hellohari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
 
@@ -37,7 +37,67 @@ public class Program
 
         else Console.WriteLine("User has NOT logged in.");
 
-        //enter credentials 
+
+        //CREATE NEW RECORD
+
+        //NAVIGATE TO TIME AND MATERIAL PAGE
+        IWebElement ADMINTAB = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+        ADMINTAB.Click();
+
+        IWebElement TMoption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+        TMoption.Click();
+
+
+        //CLICK ON CREATE NEW BUTTON 
+        IWebElement createnewbutton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+        createnewbutton.Click();
+        
+
+        //SELECT TIME FROM DROP DOWN LIST
+        IWebElement TypeCode = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[1]"));
+        TypeCode.Click();
+        IWebElement TimeOption = driver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[2]"));
+        TimeOption.Click();
+
+
+        //INPUT TYPECODE
+        IWebElement CodeTextbox = driver.FindElement(By.Id("Code"));
+        CodeTextbox.SendKeys("May2023");
+
+
+        //INPUT DESCRIPTION
+        IWebElement DescriptionTextbox = driver.FindElement(By.Id("Description"));
+        DescriptionTextbox.SendKeys("May2023");
+
+        //INPUT PRICE PER UNIT
+        IWebElement PriceTextbox = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+        PriceTextbox.SendKeys("12");
+
+
+        //CLICK ON SAVE BUTTON
+        IWebElement SaveButton = driver.FindElement(By.Id("SaveButton"));
+        SaveButton.Click();
+        Thread.Sleep(3000);
+
+        //CHK IF NEW T&m RECORD HAS BEEN CREATED SUCCESSFULLY
+
+        //navigate to last page 
+        IWebElement gotolastpagebutton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+        gotolastpagebutton.Click();
+        
+
+        //check if record is present in the table
+        IWebElement newCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+
+        if (newCode.Text == "May2023")
+
+            Console.WriteLine("Time record created successfully");
+
+        else
+            Console.WriteLine("Time record has NOT been created");
+
+
+
 
 
     }

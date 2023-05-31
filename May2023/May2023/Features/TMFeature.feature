f2db3ff18e2f0442f0ce7d650be8cb4ad26315aa
Joshua <mothercowchicken@gmail.com>
2023-05-31 15:30:51 +0800

    Specflow Implementation

---
+++ May2023/May2023/Features/TMFeature.feature
@@ -0,0 +1,13 @@
+﻿Feature: TMFeature
+
+As a TurnUp portal admin
+I wwould like to create, edit time and material records
+So that I can manage employees' time and materials successfully
+
+Scenario: Create time and material record with valid details
+	Given I logged into turnup up portal successfully
+	When I navigate to Time and Material page
+	And I create a new time and material record
+	Then The record should be created successfully
+
+
---
+++ May2023/May2023/StepDefinitions/TMFeatureStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace May2023.StepDefinitions
+{
+    [Binding]
+    public class TMFeatureStepDefinitions
+    {
+        [Given(@"I logged into turnup up portal successfully")]
+        public void GivenILoggedIntoTurnupUpPortalSuccessfully()
+        {
+            
+        }
+
+        [When(@"I navigate to Time and Material page")]
+        public void WhenINavigateToTimeAndMaterialPage()
+        {
+            
+        }
+
+        [When(@"I create a new time and material record")]
+        public void WhenICreateANewTimeAndMaterialRecord()
+        {
+            
+        }
+
+        [Then(@"The record should be created successfully")]
+        public void ThenTheRecordShouldBeCreatedSuccessfully()
+        {
+            
+        }
+    }
+}
